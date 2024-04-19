6c6eb85c6f4a6e19ccff0d0014f5571254bc367e
Saviour1001 <harshghodkar@gmail.com>
2024-04-20 01:25:23 +0530

    added contracts

---
+++ contracts/consts.ts
@@ -0,0 +1,4 @@
+export const sepoliaContractAddress = "0xccc8aFf496bCae6a05114dB4c27279d8b6D0942A";
+export const arbitrumSepoliaContractAddress = "0x6633589236aa3cc786c113a9b24d77cfb2ebf0b1";
+export const filecoinCalibrationContractAddress = "0xA4CCEb9e84b9682ca559AA41DB57f4BECe586dc5";
+
